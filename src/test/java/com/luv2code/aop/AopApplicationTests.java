0d053fb36a0c6da4875c5a87a81f8ed3df093033
yavor <jm87585@gmail.com>
2023-09-26 20:17:43 +0300

    Initial AOP project

---
+++ src/test/java/com/luv2code/aop/AopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luv2code.aop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
